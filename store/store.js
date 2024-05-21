7ed44e580fda880e18b4c6f2cf4555fba43d948c
Maja Ajhorn <95766701+majaajhorn@users.noreply.github.com>
2024-05-21 20:08:23 +0200

    Add files via upload

--- store/store.js
+++ store/store.js
@@ -1,5 +1,5 @@
-// src/store/index.js
 import { createStore } from 'vuex';
+import { ref, onMounted } from 'vue'; // Import ref and onMounted from Vue Composition API
 
 const store = createStore({
   state: {
@@ -22,3 +22,11 @@ const store = createStore({
 });
 
 export default store;
+
+const password = ref('');
+onMounted(() => {
+  const user = store.state.currentUser;
+  if (user) {
+    password.value = user.password;
+  }
+});
